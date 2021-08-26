813c34250053acaa6525c83ce261a773b31c7123
alexanderdiegocv <83038698+alexanderdiegocv@users.noreply.github.com>
2021-08-26 12:44:52 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/*const matriz = [[1,2,3],[4,5,6],[7,8,9]];
+
+let sumation_Main_Diagonal = 0;
+for (let i = 0; i < matriz.length; i++) {
+    const element = matriz[i];
+    sumation_Main_Diagonal += element[i];
+}
+
+
+let sumation_of_all_Matriz = 0;
+for (let i = 0; i < matriz.length; i++) {
+    const elements = matriz[i];
+    for (let j = 0; j < elements.length; j++) {
+        const element = elements[j];
+        sumation_of_all_Matriz += element;
+    }
+}
+
+console.log(`La suma de todos los elementos que no pertenecen a la diagonal principal de una matrix bidimensional cuadrada de 3x3 ${matriz} es ${sumation_of_all_Matriz-sumation_Main_Diagonal}`)
+*/
+
+const array = [12,8,13,9,11];
+
+
+
+
+
