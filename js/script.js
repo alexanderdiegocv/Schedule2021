c0bf83f8d8b328275da37254edfff8749f694889
alexanderdiegocv <alexanderdiego2001@outlook.com>
2021-09-16 22:17:56 -0500

    Java

--- js/script.js
+++ js/script.js
@@ -1,42 +1,47 @@
-const newSubjects = document.getElementById("newsubjects")
-const section = document.getElementById("section")
 
-newSubjects.addEventListener("submit", e => {
-    e.preventDefault() 
+const teams = [["Equipo1", "25"], ["Equipo2", "14"], ["Equipo3", "10"], ["Equipo4", "6"], ["Equipo5", "2"]]
 
-    const hour = document.getElementById("hour").value
-    const subject1 = document.getElementById("subject1").value
-    const subject2 = document.getElementById("subject2").value
-    const subject3 = document.getElementById("subject3").value
-    const subject4 = document.getElementById("subject4").value
-    const subject5 = document.getElementById("subject5").value
+console.log(teams)
 
-    section.innerHTML += `  <article class="article grid-item">
-                                <p class="dia">${hour}</p>
-                            </article>
-                            <article class="grid-item">
-                                <p class="dia">${subject1}</p>
-                            </article>
-                            <article class="grid-item">
-                                <p class="dia">${subject2}</p>
-                            </article>
-                            <article class="article grid-item radius">
-                                <p class="dia">${subject3}</p>
-                            </article>
-                            <article class="article grid-item radius">
-                                <p class="dia">${subject4}</p>
-                            </article>
-                            <article class="grid-item">
-                                <p class="dia">${subject5}</p>
-                            </article>`;
+// const newSubjects = document.getElementById("newsubjects")
+// const section = document.getElementById("section")
 
-    document.getElementById("newsubjects").classList.add('none')
-})
+// newSubjects.addEventListener("submit", e => {
+//     e.preventDefault() 
+
+//     const hour = document.getElementById("hour").value
+//     const subject1 = document.getElementById("subject1").value
+//     const subject2 = document.getElementById("subject2").value
+//     const subject3 = document.getElementById("subject3").value
+//     const subject4 = document.getElementById("subject4").value
+//     const subject5 = document.getElementById("subject5").value
+
+//     section.innerHTML += `  <article class="article grid-item">
+//                                 <p class="dia">${hour}</p>
+//                             </article>
+//                             <article class="grid-item">
+//                                 <p class="dia">${subject1}</p>
+//                             </article>
+//                             <article class="grid-item">
+//                                 <p class="dia">${subject2}</p>
+//                             </article>
+//                             <article class="article grid-item radius">
+//                                 <p class="dia">${subject3}</p>
+//                             </article>
+//                             <article class="article grid-item radius">
+//                                 <p class="dia">${subject4}</p>
+//                             </article>
+//                             <article class="grid-item">
+//                                 <p class="dia">${subject5}</p>
+//                             </article>`;
+
+//     document.getElementById("newsubjects").classList.add('none')
+// })
 
 
-const showResults = (...results) => {
-    console.log(results)
-}
+// const showResults = (...results) => {
+//     console.log(results)
+// }
 
 
 
